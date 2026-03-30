5e81f26e22c41c5707a80ed95c2733571562987e
Urban Burnik <76433162+ub-lucami@users.noreply.github.com>
2026-03-30 12:51:03 +0200

    Update start.sh

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Rendering templates..."
+echo "Rendering Nginx templates..."
+
 for tpl in /etc/nginx/templates/conf.d/*.template; do
     out="/etc/nginx/conf.d/$(basename "$tpl" .template)"
     envsubst < "$tpl" > "$out"
     echo "Generated $out"
 done
 
-echo "Starting nginx..."
-nginx -g "daemon off;"
+echo "Starting Nginx..."
+exec nginx -g "daemon off;"
